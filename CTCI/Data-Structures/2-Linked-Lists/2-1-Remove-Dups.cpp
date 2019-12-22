7e93e2c2fff18294d3e6823b5e7bd991c8067d78
Gunjan <gunjan.nandy1@gmail.com>
2019-12-22 13:51:35 +0530

    2-1-Remove-Dups

---
+++ CTCI/Data-Structures/2-Linked-Lists/2-1-Remove-Dups.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stdlib.h>
+#include<unordered_set>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+// O(n) solution using unordered_set
+void removeDuplicates(Node* head){
+    Node *curr=head, *prev=NULL, *dup;
+    unordered_set<int> duplicateTable;
+    while (curr!=NULL){
+        if (duplicateTable.find(curr->data)!=duplicateTable.end()) {
+            dup = curr;
+            prev->next = curr->next;
+            curr = curr->next;
+            delete(dup);
+        } else {
+            duplicateTable.insert(curr->data);
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    return;
+}
+
+void display(Node* head){
+    while(head->next!=NULL){
+        cout << head << " -> " << head->data << " | ";
+        head = head->next;
+    }
+    cout << endl;
+    return;
+}
+
+int main(){
+    Node* head = new Node();
+    Node* temp = head;
+    for (int i=10;i<20;i++){
+        temp->data = i/3;
+        temp->next = new Node();
+        temp = temp->next;
+    }
+    cout << "Input Linked List:" << endl;
+    display(head);
+    removeDuplicates(head);
+    cout << "After removing duplicates:" << endl;
+    display(head);
+    return 0;
+}

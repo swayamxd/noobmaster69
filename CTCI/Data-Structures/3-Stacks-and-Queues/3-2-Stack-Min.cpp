223cf3f9a770ad2317937c0dd1b4c34005d0dd93
Gunjan <gunjan.nandy1@gmail.com>
2019-12-24 13:43:10 +0530

    3-2-Stack-Min

---
+++ CTCI/Data-Structures/3-Stacks-and-Queues/3-2-Stack-Min.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stack>
+using namespace std;
+
+class minStack : stack<int>{
+    private:
+    stack<int> superStack;
+    stack<int> minStack;
+    public:
+    void push(int value){
+        if(minStack.empty() || minStack.top() > value){
+            minStack.push(value);
+        }
+        superStack.push(value);
+    }
+    void pop(){
+        if(!minStack.empty() && minStack.top() == superStack.top()){
+            minStack.pop();
+        }
+        superStack.pop();
+        return;
+    }
+    int top(){
+        if (!superStack.empty()) return superStack.top();
+        return 0;
+    }
+    int min(){
+        if (!minStack.empty()) return minStack.top();
+        return 0;
+    }
+};
+
+int main(){
+    minStack stack;
+    cout << "stack data :" << endl;
+    for (int i=0;i<10;i++){
+        int data = rand()%10;
+        // int data = 10-i;
+        cout << data << " , ";
+        stack.push(data);
+    }
+    cout << endl << "Tallying with min with stack :\nstack : min" << endl;
+    for (int i=0;i<10;i++){
+        cout << stack.top() << " : " << stack.min() << endl;
+        stack.pop();
+    }
+    return 0;
+}

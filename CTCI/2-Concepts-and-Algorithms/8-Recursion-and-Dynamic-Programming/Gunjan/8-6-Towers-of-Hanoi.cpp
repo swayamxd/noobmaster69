31c8d3fbae9b7dfa6b779d3f46c4f31be6e0eadb
Gunjan <gunjan.nandy1@gmail.com>
2020-01-16 12:25:33 +0530

    8-6-Towers-of-Hanoi

---
+++ CTCI/2-Concepts-and-Algorithms/8-Recursion-and-Dynamic-Programming/Gunjan/8-6-Towers-of-Hanoi.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+// prints a tower
+void display(stack<int> tower){
+    while(!tower.empty()){
+        cout << tower.top() << ";";
+        tower.pop();
+    }
+    cout << endl;
+}
+
+// generates a tower
+stack<int> generateTower(int diskCount){
+    stack<int> tower;
+    while(diskCount--){
+        tower.push(diskCount);
+    }
+    return tower;
+}
+
+// O(2^n) solution using recursion
+void towerOfHanoi(int n, stack<int> &source,stack<int> &buffer,stack<int> &destination){
+    // if there is no disk in source, return
+    if(n==0) return;
+    // move (n-1) disks to buffer using destination as buffer
+    // making buffer as destination, and destination as buffer
+    towerOfHanoi(n-1,source,destination,buffer);
+    // move nth disk from source to destination
+    destination.push(source.top());
+    source.pop();
+    // now source is empty as buffer was at first
+    // buffer contains all disk as source contained at first
+    // so call the function again interchanging source and buffer
+    towerOfHanoi(n-1,buffer,source,destination);
+}
+
+
+// create auxiliary and destination towers
+stack<int> towerOfHanoi(stack<int> source){
+    stack<int> buffer;
+    stack<int> destination;
+    towerOfHanoi(source.size(),source,buffer,destination);
+    return destination;
+}
+
+int main(){
+    int diskCount = 10;
+    display(towerOfHanoi(generateTower(diskCount)));
+    return 0;
+}

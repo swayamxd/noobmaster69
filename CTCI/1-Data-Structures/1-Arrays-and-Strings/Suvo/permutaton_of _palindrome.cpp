55b721af654deed4e0295545ec7e6355b5ce2447
Suvajit Roy <41955571+suvajitroy60@users.noreply.github.com>
2020-01-25 03:01:38 +0530

    Add files via upload

---
+++ CTCI/1-Data-Structures/1-Arrays-and-Strings/Suvo/permutaton_of _palindrome.cpp
@@ -0,0 +1,37 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPermutationPalindrome(string str)
+{
+    int count[128] = {0};
+    for (int i = 0; i < str.length();  i++)
+      if(str[i] != ' '){
+        count[str[i]]++;
+      }
+      
+    int odd = 0;
+    for (int i = 0; i < 128; i++)
+    {
+        if (count[i] & 1)
+            odd++;
+
+        if (odd > 1)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+  string str;
+  cout << "Enter the string" << endl;
+  getline(cin,str);
+  if (isPermutationPalindrome(str)){
+      cout << "True" << endl;
+  }
+  else {
+  	cout << "False"<< endl;
+  }
+  return 0;
+}
